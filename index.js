70eff9a6444c118ddc2998810d310a7d2698a0c5
João Aguiar Soares Teixeira <144907820+Joaoteixeira033@users.noreply.github.com>
2023-10-18 18:04:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import { c } from './c.svj';
+import { adobe_illustrator } from './adobe_illustrator.svj';
+import { css } from './css.svj';
+import { eclipse } from './eclipse.svj';
+import { intellij } from './intellij.svj';
+import { html5 } from './html5.svj';
+import { java } from './java.svj';
+import { javascript } from './javascript.svj';
+import { microsoft_sql } from './microsoft_sql.svj';
+import { mysql } from './mysql.svj';
+import { netlify } from './netlify.svj';
+import { next_js } from './next_js.svj';
+import { nodejs } from './nodejs.svj';
+import { pycharm } from './pycharm.svj';
+import { python } from './python.svj';
+import { react } from './react.svj';
+import { typescript } from './typescript.svj';
+import { visual_studio_code } from './visual_studio_code.svj';
+
+
+
+
+export default{
+    c,
+    adobe_illustrator,
+    css,
+    eclipse,
+    intellij,
+    html5,
+    java,
+    javascript,
+    microsoft_sql,
+    mysql,
+    netlify,
+    next_js,
+    nodejs,
+    pycharm,
+    python,
+    react,
+    typescript,
+    visual_studio_code,
+}
